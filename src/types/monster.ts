19585e9bb7643369f9a67ee7a53bc316737cb8d9
Bradley Goodyear <mustyoshi@gmail.com>
2024-07-23 20:35:38 -0400

    Update Monster schema to match wiki infobox

--- src/types/monster.ts
+++ src/types/monster.ts
@@ -1,3 +1,29 @@
+export interface MonsterCombatStats {
+  attack: number;
+  strength: number;
+  defence: number;
+  magic: number;
+  ranged: number;
+
+  attackBonus: number;
+  strengthBonus: number;
+  attackMagic: number;
+  magicBonus: number;
+
+  attackRanged: number;
+  rangedBonus: number;
+  defenceStab: number;
+  defenceSlash: number;
+  defenceCrush: number;
+  defenceMagic: number;
+  defenceRanged: number;
+
+  immunePosion: boolean;
+  immuneVenom: boolean;
+  immuneCannon: boolean;
+  immuneThrall: boolean;
+}
+
 export interface MonsterDrop {
   name: string;
   itemId: number | null;
@@ -7,8 +33,30 @@ export interface MonsterDrop {
 
 export interface Monster {
   id: number;
+  ids: number[];
+
+  version: string;
   name: string;
+  image: string;
+  release: string;
+  update: string;
+  members: boolean;
+  level: number; // combat
+  size: number;
   examine: string;
+  xpBonus: number;
+  maxHit: number;
+  aggressive: boolean;
+  poisonous: boolean;
+  attackStyle: string;
+  attackSpeed: number;
+  slayXp: number;
+  category: string;
+  hitpoints: number;
+  assignedBy: string[];
+  combatStats: MonsterCombatStats;
+  respawnTime: number;
+
   aliases: string[];
   drops: MonsterDrop[];
 }
