aab6477406aa6e6e5944605ac97d0ce4efbe4bec
Antony Garand <9059487+AntonyGarand@users.noreply.github.com>
2025-04-16 21:18:31 -0400

    Added other config stuff I forgot before

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/modules/database/schema.ts',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: process.env.DB_PATH,
+  },
+});
